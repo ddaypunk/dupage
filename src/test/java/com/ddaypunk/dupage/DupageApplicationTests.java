b5365a38512c962c70880b6b281ce71dd626b9ad
Andy Delso <andy@delso.me>
2020-05-31 21:28:11 -0500

    add Log4J2 to the project with example in test

--- src/test/java/com/ddaypunk/dupage/DupageApplicationTests.java
+++ src/test/java/com/ddaypunk/dupage/DupageApplicationTests.java
@@ -1,6 +1,8 @@
 package com.ddaypunk.dupage;
 
 import com.ddaypunk.dupage.core.DupageApplication;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -17,15 +19,18 @@ class DupageApplicationTests {
 
 	@Autowired
 	private WebDriver driver;
+	private static final Logger LOGGER = LogManager.getLogger("test");
 
 	@Test
 	void contextLoads() {
 		var url = "https://google.com"; // Java 10: Local type inference
 
 		// navigate to page
+		LOGGER.info("Navigating to test page");
 		driver.get(url);
 
 		// Find search input and utilize it
+		LOGGER.info("Getting search input and entering search query");
 		var googleSearchInput = driver.findElement(By.cssSelector("[title='Search']"));
 		googleSearchInput.sendKeys("Star Wars");
 		googleSearchInput.sendKeys(Keys.RETURN);
@@ -35,8 +40,10 @@ class DupageApplicationTests {
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("result-stats")));
 
 		// assert we are on the search view
+		LOGGER.info("Checking for results page...");
 		assertTrue(driver.getCurrentUrl().contains("search")
 				&& driver.getCurrentUrl().contains("Star+Wars"));
+		LOGGER.info("Results page found!!!");
 	}
 
 }
