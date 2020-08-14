341bd775655c40938b27f3b95564eef32c20b5f8
Andy Delso <andy@delso.me>
2020-08-13 20:09:53 -0500

    add early version of Wait builder wrapper

---
+++ src/main/java/com/ddaypunk/dupage/core/Wait.java
@@ -0,0 +1,63 @@
+package com.ddaypunk.dupage.core;
+
+import com.ddaypunk.dupage.core.enums.Condition;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.Duration;
+
+public class Wait {
+    @Autowired
+    private WebDriver driver;
+
+    private Condition condition;
+    private By selector;
+    private Duration duration;
+
+
+    public static final class Builder {
+        private Condition condition;
+        private By selector;
+        private Duration duration;
+
+        public Builder _for(Condition condition) {
+            this.condition = condition;
+            return this;
+        }
+
+        public Builder of(By selector) {
+            this.selector = selector;
+            return this;
+        }
+
+        public Builder until(Duration duration) {
+            this.duration = duration;
+            return this;
+        }
+
+        public Wait build() {
+            Wait wait = new Wait();
+            wait.condition = this.condition;
+            wait.duration = this.duration;
+            wait.selector = this.selector;
+            return wait;
+        }
+    }
+
+    public WebElement start() {
+        return switch(condition) {
+            case VISIBILITY -> waitForVisOf();
+            // TODO: Throwing this for now until other branches are complete
+            default -> throw new IllegalStateException("Unexpected value: " + condition);
+        };
+    }
+
+    private WebElement waitForVisOf() {
+        WebDriverWait wait = new WebDriverWait(driver, duration.toSeconds());
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(selector));
+    }
+}
